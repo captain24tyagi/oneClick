b7b1789e1a87a69dd0a99be14fc9495a015f7f9e
captain24tyagi <sarthak2002tyagi@gmail.com>
2023-01-31 12:02:50 +0530

    odpage

---
+++ pages/onDuty.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function onDuty() {
+  return (
+    <div className='h-screen text-center items-center justify-between max-w-full p-10 bg-[url("/imageb.png")]'>
+      <div className=''>
+        <h1 className='px-7 text-left py-5 h-fit w-fit bg-orange-400 rounded-md text-xl font-bold text-white'>
+            Application for OD 
+        </h1>
+        <form>
+
+        </form>
+      </div>
+
+    </div>
+  )
+}
+
+export default onDuty
