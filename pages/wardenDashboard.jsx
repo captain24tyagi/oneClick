6fbb696959b36c08b9c756ed318fa8984e54146b
captain24tyagi <sarthak2002tyagi@gmail.com>
2023-02-01 08:56:16 +0530

    wardendashboard

--- pages/wardenDashboard.jsx
+++ pages/wardenDashboard.jsx
@@ -1,41 +1,50 @@
-import StudDashboardCard from "@/components/StudDashboardCard"
-import React, { useEffect, useState } from "react"
+import React from 'react'
+import { useSignMessage } from "wagmi"
+import { verifyMessage } from "ethers/lib/utils"
+import WardenCard from '@/components/WardenCard'
 
-function studentDashboard() {
-  const [odMl, setOdMl] = useState(null)
+function wardenDashboard() {
+
+  const [odMls, setOdMls] = useState(null)
+
+  const recoveredAddress = useRef()
+
+  const { data, error, isLoading, signMessage } = useSignMessage({
+    onSuccess(data, variables) {
+      const address = verifyMessage(variables.message, data)
+      recoveredAddress.current = address
+    },
+  })
+
   useEffect(() => {
-    const data = { netId: "dadsd" }
-
-    fetch("http://localhost:5000/viewOdMl", {
-      method: "POST", // or 'PUT'
+    fetch("http://localhost:5000/viewOdMls", {
+      method: "GET", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
     })
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data)
-        setOdMl(data)
+        setOdMls(data)
       })
       .catch((error) => {
         console.error("Error:", error)
       })
   }, [])
+
   return (
     <div className='min-h-screen bg-[url("/imageb.png")]'>
-
       <div className=" flex-1 max-w-6xl mx-auto p-10">
         <h1 className="px-7 text-left py-3 h-fit w-fit bg-blue-500 rounded-t-lg text-xl font-bold text-white scrollbar scrollbar-track-black/20 scrollbar-thumb-[#93C5FD]">
-          Student Approval Dashboard
+          Warden Approval Dashboard
         </h1>
 
         <div>
           <div className=" bg-black/25 min-h-screen rounded-t-lg p-10 flex gap-7 overflow-x-scroll">
-
-            {odMl &&
-              odMl.map((data) => (
-                <HostelCard
+            {odMls &&
+              odMls.map((data) => (
+                <WardenCard
                   key={data._id}
                   netId={data.netId}
                   title={data.title}
@@ -51,4 +60,4 @@ function studentDashboard() {
   )
 }
 
-export default studentDashboard
+export default wardenDashboard
