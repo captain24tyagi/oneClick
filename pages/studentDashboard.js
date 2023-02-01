4ef779a8fe5130435da58366f91064e22f32a100
datta-soumil <soumil13@gmail.com>
2023-02-01 07:44:25 +0530

    fix-issues

--- pages/studentDashboard.js
+++ pages/studentDashboard.js
@@ -23,14 +23,16 @@ function studentDashboard() {
       })
   }, [])
   return (
-    <div className='h-full bg-[url("/imageb.png")]'>
+    <div className='min-h-screen bg-[url("/imageb.png")]'>
+
       <div className=" flex-1 max-w-6xl mx-auto p-10">
         <h1 className="px-7 text-left py-3 h-fit w-fit bg-blue-500 rounded-t-lg text-xl font-bold text-white scrollbar scrollbar-track-black/20 scrollbar-thumb-[#93C5FD]">
-          Student Dashboard
+          Student Approval Dashboard
         </h1>
 
         <div>
-          <div className=" bg-black/25 min-h-screen rounded-t-lg p-10 flex gap-7 overflow-x-scroll ">
+          <div className=" bg-black/25 min-h-screen rounded-t-lg p-10 flex gap-7 overflow-x-scroll">
+
             {odMl &&
               odMl.map((data) => (
                 <StudDashboardCard
